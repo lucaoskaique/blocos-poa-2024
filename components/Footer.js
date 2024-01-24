30e26b5cbf0bc75ceef0422f496f6b3a1abd98a0
lucaoskaique <lucas.kaique175@hotmail.com>
2024-01-24 01:07:27 -0300

    feat: added name to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => (
     <footer className="flex flex-col items-center justify-center">
-        <div className="font-sora w-auto px-2 md:px-5 py-5 mt-20
+        <div
+            className="font-sora w-auto px-2 md:px-5 py-5 mt-20
                 inline-flex justify-between md:justify-center items-center
                 bg-light-gray rounded-t-lg"
         >
             <p className="text-center text-xs md:text-base font-light">
-                Copyright &copy; lucaoskaique
+                Copyright &copy;
+                {' '}
+                <Link href="https://lucaskaique.com.br">lucaoskaique</Link>
                 {' '}
                 {new Date().getFullYear()}
             </p>
         </div>
     </footer>
-);
+)
 
-export default Footer;
+export default Footer
